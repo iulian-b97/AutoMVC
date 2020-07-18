27745b0be0bd4f9837bbe99e507bfb40b9e39a2b
iulianb97 <iulian.burghelea1997@gmail.com>
2020-07-18 15:10:32 +0300

    Second commit

---
+++ Controllers/MyAccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMVC.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+    }
+}
