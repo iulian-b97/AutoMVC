278b99bb432e5a9c7daea0b0eb0e27cf6625ca4f
unknown <iulian.burghelea1997@gmail.com>
2021-11-06 10:34:04 +0200

    Implement empty AdminController patch

---
+++ Server/IdentityManagement.Api/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        public AdminController()
+        {
+
+        }
+    }
+}
